dc29213119712e823bf1165d0cf010c409bd7ead
JuanRodLes <juan1503esteban@gmail.com>
2023-01-27 21:27:37 -0500

    AvanceClase

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -6,10 +6,27 @@ function pokeApi(url = "https://pokeapi.co/api/v2/pokemon") {
             for (const pokemon of data1.results) {
                 fetch(pokemon.url)
                     .then(res2 => res2.json())
-                    .then(dataPokimon2 => {
-                        console.log(dataPokimon2.sprites.other['official-artwork'].front_default)
+                    .then(data2 => {debugger
+                        for (const IStats of data2) {
+                            fetch(IStats.stats[0].stat.url)
+                                .then(res3 => res3.json())
+                                .then(data3 => {
+                                    for (const ILang of data3) {
+                                        fetch(ILang.names[5].name)
+                                        .then(res4 => res4.json())
+                                        .then(data4 => {
+                                            console.log(data4)
+                                            
+                                        })//4to data//
+                                    }//3er ForOf//
+                                })//3er data//
+                        }//2do ForOf//
+                        // console.log(dataPokimon2.sprites.other['official-artwork'].front_default) //camino para la img
                     }) //2do data//
-            }//ForOf//
+            }//1er ForOf//
         })//1er data//
 }//function//
 pokeApi()
+
+
+
