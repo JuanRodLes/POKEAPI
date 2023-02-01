36f5c16979f655f653ad769e75636917cb6b17fd
JuanRodLes <juan1503esteban@gmail.com>
2023-01-31 21:26:44 -0500

    Variables Unidas

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,24 +1,26 @@
 function pokeApi(url = "https://pokeapi.co/api/v2/pokemon") {
-
+    
     let consumoApi = fetch(url)
     consumoApi.then(res => res.json())
-        .then(dataPokemon1 => {
-            for (const pokemon of dataPokemon1.results) {
-                fetch(pokemon.url)
-                    .then(res2 => res2.json())
-                    .then(dataPokemon2 => {
-                        for (const StatsPokemon of dataPokemon2.stats) {
-                            fetch(StatsPokemon.stat.url)
-                                .then(res3 => res3.json())
-                                .then(dataStats => {
-                                    for (const LangStats of dataStats.names) {
+    .then(dataPokemon1 => {
+        for (const pokemon of dataPokemon1.results) {
+            fetch(pokemon.url)
+            .then(res2 => res2.json())
+            .then(dataPokemon2 => {
+                for (const StatsPokemon of dataPokemon2.stats) {
+                    fetch(StatsPokemon.stat.url)
+                    .then(res3 => res3.json())
+                    .then(dataStats => {
+                        for (const LangStats of dataStats.names) {
+                                        
+                                        let PS = (LangStats.name == "PS") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
+                                        let AT = (LangStats.name == "Ataque") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
+                                        let DF = (LangStats.name == "Defensa") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
+                                        let EAT = (LangStats.name == "Ataque Especial") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
+                                        let EDF = (LangStats.name == "Defensa Especial") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
+                                        let VEL = (LangStats.name == "Velocidad") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
+                                        let allStats = PS+AT+DF+EAT+EDF+VEL
                                         if (LangStats.language.name == "es") {
-                                            let PS = (LangStats.name == "PS") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
-                                            let AT = (LangStats.name == "Ataque") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
-                                            let DF = (LangStats.name == "Defensa") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
-                                            let EAT = (LangStats.name == "Ataque Especial") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
-                                            let EDF = (LangStats.name == "Defensa Especial") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
-                                            let VEL = (LangStats.name == "Velocidad") ? `${LangStats.name}: ${StatsPokemon.base_stat}` : "";
 
                                             document.querySelector("#cartaPokemon").innerHTML += `
                                             <div class="card bg-dark m-2" style="width: 18rem;">
@@ -29,26 +31,26 @@ function pokeApi(url = "https://pokeapi.co/api/v2/pokemon") {
                                                 <table class="table table-dark border-top d-flex justify-content-around">
                                                     <tbody>
                                                         <tr>
-                                                            <td>${PS}</td>
-                                                            <td>${AT}</td>
+                                                            <td>${allStats}</td>
+                                                            <td>${allStats}</td>
                                                         </tr>
                                                         <tr>
-                                                            <td>${DF}</td>
-                                                            <td>${EAT}</td>
+                                                            <td>${allStats}</td>
+                                                            <td>${allStats}</td>
                                                         </tr>
                                                         <tr>
-                                                            <td>${EDF}</td>
-                                                            <td>${VEL}</td>
+                                                            <td>${allStats}</td>
+                                                            <td>${allStats}</td>
                                                         </tr>
                                                     </tbody>
                                                 </table>
                                             </div>`
-
+                                            return
+                                            debugger
                                         }//condicional//
                                     } //3er ForOf//
                                 })//3er dataStats//
                         }//2do ForOf//
-                        // console.log(dataPokemon2.sprites.other['official-artwork'].front_default)
                     }) //2do dataPokemon2//
             }//1er ForOf//
         })//1er dataPokemon1//
